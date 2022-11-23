24b905667ae613914d65b451f9fe3a598f33898d
Amy Kaminski <amykaminski928@gmail.com>
2022-11-23 01:45:20 -0600

    created classes using UML

---
+++ battlefield.py
@@ -0,0 +1,19 @@
+from robot import Robot
+from dinosaur import Dinosaur
+
+class Battlefield:
+    def __init__(self):
+        active_robot=Robot()
+        active_dinosaur=Dinosaur()
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
---
+++ robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name):
+        self.name=""
+        self.health=int
+        self.active_weapon=""
+
+    def attack(self, dinosaur):
+        pass
