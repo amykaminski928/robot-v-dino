fc7f2a20258e78c3be0a2e9f9dddd7df875046b7
Amy Kaminski <amykaminski928@gmail.com>
2022-11-25 11:30:24 -0600

    debugged robot attack

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from dinosaur import Dinosaur
 from weapon import Weapon
 from battlefield import Battlefield
 #instantiate weapon
-available_weapon=Weapon("Fangs", 5)
+#active_weapon=Weapon("Fangs", 5)
 #instantiate robot
 active_robot=Robot("Chewy")
-active_weapon=available_weapon
+
 
 #instantiate Dinosaur
 active_dino=Dinosaur("Blue", 10)
@@ -14,3 +14,4 @@ active_dino=Dinosaur("Blue", 10)
 
 #dino attack
 active_dino.attack(active_robot)
+active_robot.attack(active_dino)
--- robot.py
+++ robot.py
@@ -4,7 +4,9 @@ class Robot:
     def __init__(self, name):
         self.robot_name=name
         self.robot_health=25
-        self.active_weapon=Weapon(self, name)
+        self.active_weapon=Weapon('fangs', 5)
 
     def attack(self, dinosaur):
-        pass
+        dinosaur.dino_health-=self.active_weapon.weapon_attack_power
+        print(f'{dinosaur.dino_name} has {dinosaur.dino_health} health remaining.')
+        
